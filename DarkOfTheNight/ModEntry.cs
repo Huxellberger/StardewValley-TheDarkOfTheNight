20127534938f241d06b5a56916c8aeb2feca1b2f
Jake Huxell <darkduckmovies@gmail.com>
2018-01-04 23:58:54 +0000

    Basic stuff for the mod

---
+++ DarkOfTheNight/ModEntry.cs
@@ -0,0 +1,15 @@
+﻿// Jake Huxell 2018
+
+using StardewModdingAPI;
+
+namespace DarkOfTheNight
+{
+    public class ModEntry
+        : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("Mod has loaded");
+        }
+    }
+}
