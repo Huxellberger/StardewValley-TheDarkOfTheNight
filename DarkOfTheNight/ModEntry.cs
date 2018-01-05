ee18a6e6dec3f4d4f5a4fb4805b9e12ad7691edf
Jake Huxell <darkduckmovies@gmail.com>
2018-01-05 00:27:40 +0000

    Take damage during the day

--- DarkOfTheNight/ModEntry.cs
+++ DarkOfTheNight/ModEntry.cs
@@ -1,15 +1,21 @@
 ﻿// Jake Huxell 2018
 
+using DarkOfTheNight.Vampire;
 using StardewModdingAPI;
+using StardewValley;
 
 namespace DarkOfTheNight
 {
     public class ModEntry
         : Mod
     {
+        private VampireCharacter _currentVampire;
+
         public override void Entry(IModHelper helper)
         {
             Monitor.Log("Mod has loaded");
+
+            _currentVampire = new VampireCharacter(Game1.player, 10.0f);
         }
     }
 }
---
+++ DarkOfTheNight/Vampire/VampireCharacter.cs
@@ -0,0 +1,43 @@
+﻿// Jake Huxell 2018
+
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace DarkOfTheNight.Vampire
+{
+    internal class VampireCharacter
+    {
+        public float SunDamagePerCycle { get; private set; }
+
+        private readonly StardewValley.Farmer _underlyingCharacter;
+        private bool _outside;
+
+        public VampireCharacter(StardewValley.Farmer inCharacter, float inSunDamagePerCycle)
+        {
+            _underlyingCharacter = inCharacter;
+            _outside = false;
+            SunDamagePerCycle = inSunDamagePerCycle;
+
+            LocationEvents.CurrentLocationChanged += OnLocationChanged;
+            TimeEvents.TimeOfDayChanged += OnTimeOfDayChanged;
+        }
+
+        private bool IsDarkness()
+        {
+            return Game1.isDarkOut();
+        }
+
+        private void OnLocationChanged(object sender, EventArgsCurrentLocationChanged eventArgs)
+        {
+            _outside = eventArgs.NewLocation.isOutdoors;
+        }
+
+        private void OnTimeOfDayChanged(object sender, EventArgsIntChanged inTime)
+        {
+            if (_outside && IsDarkness())
+            {
+                _underlyingCharacter.stamina -= SunDamagePerCycle;
+            }
+        }
+    }
+}
